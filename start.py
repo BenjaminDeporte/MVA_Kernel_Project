05a8874eb412b895ee557fd08332867c8c4ee3b4
BenjaminDeporte <deporte.benjamin@gmail.com>
2025-03-08 05:46:15 +0100

    Acc 64% avec Kernel spectrum + clf scikit

--- start.py
+++ start.py
@@ -10,16 +10,22 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from src.dlip.kernels import KernelSpectrum
+from src.kernels import KernelSpectrum
 from sklearn.metrics import accuracy_score
+import os
 
 # Test
 from sklearn.svm import SVC
 
-data_dir = '/home/benjamin/Folders_Python/MVA_Kernel_Project/data/raw/'
+# get data
+current_dir = os.getcwd()
 
-filename = '/home/benjamin/Folders_Python/MVA_Kernel_Project/data/raw/Xtr0.csv'
-labelname = '/home/benjamin/Folders_Python/MVA_Kernel_Project/data/raw/Ytr0.csv'
+data_dir = current_dir + '/data/'
+filename = data_dir + 'Xtr0.csv'
+labelname = data_dir + 'Ytr0.csv'
+
+# filename = '/home/benjamin/Folders_Python/MVA_Kernel_Project/data/raw/Xtr0.csv'
+# labelname = '/home/benjamin/Folders_Python/MVA_Kernel_Project/data/raw/Ytr0.csv'
 
 with open(filename,'r') as f:
     X = pd.read_csv(f, index_col=0)
@@ -27,10 +33,12 @@ with open(filename,'r') as f:
 with open(labelname, 'r') as g:
     y = pd.read_csv(g, index_col=0)
 
-k = 5
+# instantiate kernel
+k = 3
 ks = KernelSpectrum(k=k)
 kernel = ks.k_value
 
+# go Forest, go
 clf = SVC(kernel='precomputed')
 
 N = 500
@@ -57,4 +65,3 @@ print(f"Computing prediction and accuracy")
 y_pred = clf.predict(gramt)
 
 print(f"Accuracy = {accuracy_score(y_pred, y_test)*100:.1f}%")
-
